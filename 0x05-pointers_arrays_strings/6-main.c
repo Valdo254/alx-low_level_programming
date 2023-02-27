5efa8197ff044318af4abea72287877eb82a625e
Valdo254 <wazirialli254@gmail.com>
2023-02-27 08:45:06 -0800

    task 6

---
+++ 0x05-pointers_arrays_strings/6-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *str;
+
+    str = "0123456789";
+    puts2(str);
+    return (0);
+}
+julien@ubuntu:~/0x05$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 6-main.c 6-puts2.c -o 6-puts2
+julien@ubuntu:~/0x05$ ./6-puts2 
+02468
