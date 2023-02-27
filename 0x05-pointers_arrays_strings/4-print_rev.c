2054d306e0418cedc2120db8f3feddd9dd776ed7
Valdo254 <wazirialli254@gmail.com>
2023-02-27 11:15:39 -0800

    several tasks

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev - prints a string in reverse
+ * followed by a new line
+ * @s: the string to reverse
+ * Return: void
+ */
+void print_rev(char *s)
+{
+int len = strlen(s);
+
+while (len > -1)
+{
+if (s[len] != '\0')
+{
+_putchar(s[len]);
+};
+len--;
+};
+_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,22 +1,23 @@
 #include "main.h"
+#include <string.h>
 
 /**
- * puts2 - prints xters of a string
- * @str: the used string reference
- * Return: 0
+ * puts2 - prints every other character of a string
+ * starting with the first character, followed by a new line.
+ * @str: the string
+ * Return: void
  */
-
 void puts2(char *str)
 {
-	int i = 0;
+	int c = 0;
 
-	while (str[i] != '\0')
+	while (str[c] != '\0')
 	{
-		if (i % 2 == 0)
+		if (c % 2 == 0)
 		{
-			_putchar(str[i]);
+			putchar(str[c]);
 		}
-		i++;
+		c++;
 	}
-	_putchar('\n');
+	putchar('\n');
 }
