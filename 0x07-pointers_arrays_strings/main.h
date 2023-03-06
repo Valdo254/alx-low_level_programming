072021e254fe07c8862c7e153ba81d790d67f773
Valdo254 <wazirialli254@gmail.com>
2023-03-06 06:05:23 -0800

    task 0

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,10 +3,10 @@
 
 #include <stddef.h>
 
-char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *s, char c);
+int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
-int _putchar(char c)
+void *_memcpy(void *dest, const void *src, size_t n);
+char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
